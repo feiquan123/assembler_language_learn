2eba34aae56ff251b80a736b1eac6527f25fa19b
feiquan123 <feiquanxx@gmail.com>
2025-02-20 12:59:04 +0800

    更新 p9_3

--- chatpter9_transfer_Instructions/p9_3.asm
+++ chatpter9_transfer_Instructions/p9_3.asm
@@ -4,11 +4,25 @@ codeseg segment
 start:
 	mov ax,0
 	mov bx,0
-	jmp far ptr s
+	jmp far ptr s ; 段地址，偏移地址
 	db 256 dup (0)
 s: 
 	add ax,1
 	inc ax
+
+	; 转移地址在内存中
+	mov ax,offset s0
+	mov ds:[0],ax
+	jmp word ptr ds:[0]  ; 偏移地址
+	db 100 dup (0)
+
+s0:
+	; 内存 - 段间转移
+	mov ax,0123h
+	mov ds:[0],ax	; 偏移地址
+	mov word ptr ds:[2],0  ; 段地址 
+	jmp dword ptr ds:[0] ; 段地址，偏移地址
+	db 256 dup (0)
 
 	mov ax,4c00h
 	int 21h
